b9f8475eb4f4ae5d2a051f419f090b15fe3309a3
ElkanRoelen <roelen@elkan.nl>
2019-03-08 13:44:54 +0100

    Updated

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 $(aws ecr get-login --no-include-email --region eu-west-1)
 
 docker build --no-cache -t smilo/testnet-faucet:frontend -f ./frontend.Dockerfile .
-docker tag smilo/testnet-website:latest 462619610638.dkr.ecr.eu-west-1.amazonaws.com/smilo/testnet-faucet:frontend
+docker tag smilo/testnet-faucet:frontend 462619610638.dkr.ecr.eu-west-1.amazonaws.com/smilo/testnet-faucet:frontend
 docker push 462619610638.dkr.ecr.eu-west-1.amazonaws.com/smilo/testnet-faucet:frontend
 
 docker build --no-cache -t smilo/testnet-faucet:backend -f ./backend.Dockerfile .
-docker tag smilo/testnet-website:latest 462619610638.dkr.ecr.eu-west-1.amazonaws.com/smilo/testnet-faucet:backend
+docker tag smilo/testnet-faucet:backend 462619610638.dkr.ecr.eu-west-1.amazonaws.com/smilo/testnet-faucet:backend
 docker push 462619610638.dkr.ecr.eu-west-1.amazonaws.com/smilo/testnet-faucet:backend
